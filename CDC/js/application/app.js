75dc5019fbc1f8d96386a28d17b889951520bdb2
Justin P. Peters <jppeters@iu.edu>
2019-06-27 15:19:33 -0400

    save

--- CDC/js/application/app.js
+++ CDC/js/application/app.js
@@ -499,8 +499,8 @@ define([
             type: "oid"
           },
           {
-            name: "rank",
-            alias: "Rank",
+            name: "value",
+            alias: "value",
             type: "integer"
           }
         ],
@@ -537,7 +537,7 @@ define([
 
       };
       this.addHighlightedGraphic = (feature, rank) => {
-        const highlightedGraphic = new Graphic({ geometry: feature.geometry.clone(), attributes: { rank: rank } });
+        const highlightedGraphic = new Graphic({ geometry: feature.geometry.clone(), attributes: { value: value } });
         top10Layer.source.add(highlightedGraphic);
       };
       this.clearGraphics = () => {
@@ -737,7 +737,7 @@ define([
     updateVulnerableList: function (view, overallLayer, searchArea, themeName, selectFirst) {
 
      // const searchField = "RPL_" + themeName;
-      const searchField = "Total";
+      const searchField = "value";
       const listNode = themeName + "-list";
       const isActive = domClass.contains(dom.byId(themeName + "-btn"), "is-active");
 
@@ -776,7 +776,7 @@ define([
         });
 
         if(isActive && selectFirst) {
-          //vulnerableItemNodes[0].click();
+          vulnerableItemNodes[0].click();
         }
 
       });
@@ -798,9 +798,9 @@ define([
 
 
       dom.byId("selected-item-label").innerHTML = feature.getAttribute("NAME_L");
-      dom.byId("selected-item-score").innerHTML = feature.getAttribute("Theme1").toFixed(3);
+      dom.byId("selected-item-score").innerHTML = feature.getAttribute("Total").toFixed(3);
 
-      dom.byId("selected-item-pop").innerHTML = number.format(+feature.getAttribute("Theme1"));
+      dom.byId("selected-item-pop").innerHTML = number.format(+feature.getAttribute("Total"));
       dom.byId("selected-item-day-pop").innerHTML = number.format(+feature.getAttribute("Theme2"));
       dom.byId("selected-item-hu").innerHTML = number.format(+feature.getAttribute("Theme3"));
       dom.byId("selected-item-hh").innerHTML = number.format(+feature.getAttribute("Theme4"));
@@ -810,7 +810,7 @@ define([
         return (themeName !== "THEMES");
       }).map((themeName) => {
         const theme = this.SVIThemeInfos[themeName];
-        const value = +feature.getAttribute("Theme1");
+        const value = +feature.getAttribute("value");
         //const flags = +feature.getAttribute("F_" + themeName);
         return {
           y: value,
