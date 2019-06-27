97cd9c1f04a0732d8cf15f16b040a6afabd710ee
Justin P. Peters <jppeters@iu.edu>
2019-06-27 15:17:19 -0400

    save

--- CDC/js/application/app.js
+++ CDC/js/application/app.js
@@ -1,4 +1,4 @@
-e/*
+/*
  | Copyright 2016 Esri
  |
  | Licensed under the Apache License, Version 2.0 (the "License");
@@ -737,7 +737,7 @@ define([
     updateVulnerableList: function (view, overallLayer, searchArea, themeName, selectFirst) {
 
      // const searchField = "RPL_" + themeName;
-      const searchField = "Total";
+      const searchField = "value";
       const listNode = themeName + "-list";
       const isActive = domClass.contains(dom.byId(themeName + "-btn"), "is-active");
 
@@ -800,7 +800,7 @@ define([
       dom.byId("selected-item-label").innerHTML = feature.getAttribute("NAME_L");
       dom.byId("selected-item-score").innerHTML = feature.getAttribute("Total").toFixed(3);
 
-      dom.byId("selected-item-pop").innerHTML = number.format(+feature.getAttribute("Theme1"));
+      dom.byId("selected-item-pop").innerHTML = number.format(+feature.getAttribute("Total"));
       dom.byId("selected-item-day-pop").innerHTML = number.format(+feature.getAttribute("Theme2"));
       dom.byId("selected-item-hu").innerHTML = number.format(+feature.getAttribute("Theme3"));
       dom.byId("selected-item-hh").innerHTML = number.format(+feature.getAttribute("Theme4"));
@@ -810,7 +810,7 @@ define([
         return (themeName !== "THEMES");
       }).map((themeName) => {
         const theme = this.SVIThemeInfos[themeName];
-        const value = +feature.getAttribute("Total");
+        const value = +feature.getAttribute("value");
         //const flags = +feature.getAttribute("F_" + themeName);
         return {
           y: value,
