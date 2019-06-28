9710db983f494e0f396148431e51ae03ea5a6315
Justin P. Peters <jppeters@iu.edu>
2019-06-28 08:50:20 -0400

    save

--- CDC/js/application/app.js
+++ CDC/js/application/app.js
@@ -537,7 +537,7 @@ define([
 
       };
       this.addHighlightedGraphic = (feature, rank) => {
-        const highlightedGraphic = new Graphic({ geometry: feature.geometry.clone(), attributes: { value: value } });
+        const highlightedGraphic = new Graphic({ geometry: feature.geometry.clone(), attributes: { value: Total } });
         top10Layer.source.add(highlightedGraphic);
       };
       this.clearGraphics = () => {
